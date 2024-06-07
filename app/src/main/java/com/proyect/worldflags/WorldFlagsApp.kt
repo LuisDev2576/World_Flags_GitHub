497ec24c389685f3ff8cee6c92c8662f8af8c51a
Luis Daniel Martínez Barrios <lmartinez@wi-mobile.com>
2024-06-07 13:54:21 -0500

    Miltidex agregado al proyecto
    Signed-off-by: Luis Daniel Martínez Barrios <lmartinez@wi-mobile.com>

--- app/src/main/java/com/proyect/worldflags/WorldFlagsApp.kt
+++ app/src/main/java/com/proyect/worldflags/WorldFlagsApp.kt
@@ -1,7 +1,7 @@
 package com.proyect.worldflags
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class WorldFlagsApp: Application()
+class WorldFlagsApp: MultiDexApplication()
